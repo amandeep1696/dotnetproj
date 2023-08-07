363284eeaae8940d6232e6ad115211b57fdc95cd
Amandeep Singh <am418470@dal.ca>
2023-08-07 14:13:47 -0300

    Scaffold basic frontend, integrate and handle CORS

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy => 
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
 
 
 
@@ -24,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
